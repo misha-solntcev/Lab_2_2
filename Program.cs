973bd631a8700a4de9b741730d54dd2d4e9d7d32
misha-solntcev <misha.solntcev@gmail.com>
2022-11-17 13:43:09 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Массив коэфиициентов четного полинома Вандермонда.
+            double[] V = new double[]
+            { +1, -0.1666666664, +0.0083333315, -0.0001984090, +0.0000027526, -0.0000000239 };
+                        
+            double z = 90;          // Задаем градусы
+            double r = Radian(z);   // Переводим градусы в радианы.
+            double sum = 0;
+            for (int i = 0; i < V.Length; i++)            
+                sum += V[i] * Math.Pow(r, 2 * i);
+
+            Console.WriteLine($"Синус {z} градусов = {sum * r}");
+            Console.ReadKey();           
+        }
+
+        // Функция перевода градусов в радианы
+        static double Radian(double z)
+        {
+            double result = z * Math.PI / 180;
+            return result;
+        }
+    }
+}
